4f3e2a1a414d1835a2724ae02aeece1f5722904a
Benito Lopez <bennobenno2730@gmail.com>
2023-07-09 11:16:16 -0600

    insertando items a base de datos de mongo

--- app.js
+++ app.js
@@ -2,13 +2,48 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require("body-parser");
+const mongoose=require("mongoose")
 
-var agrega="";
-var items=[];
+var agrega;
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"))
 app.set('view engine','ejs')
+mongoose.connect("mongodb://127.0.0.1:27017/todoListDB")
+
+
+const itemsSchema={
+  name:String,
+}
+
+
+
+const  Items=mongoose.model("item",itemsSchema)
+const item1= new Items({
+
+  name:"Programming"
+})
+
+const item2=new Items({
+  name :"Eat"
+})
+const item3=new Items({
+  name:"Study"
+})
+const defaulItems=[item1,item2,item3]
+
+const insertItems=async()=>{
+try {
+  await Items.insertMany(defaulItems);
+  console.log("Éxito");
+} catch (error) {
+  console.log("Error", error);
+}
+}
+insertItems()
+
 
 app.get('/', (req, res) => {
   
